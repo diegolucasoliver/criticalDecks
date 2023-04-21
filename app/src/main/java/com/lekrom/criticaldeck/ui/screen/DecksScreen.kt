5c14903704dce5d876755d548f902c90535a94b4
Diego Oliveira <diegolucasoliver@hotmail.com>
2023-04-21 14:28:12 -0300

    added a title in decks screen

--- app/src/main/java/com/lekrom/criticaldeck/ui/screen/DecksScreen.kt
+++ app/src/main/java/com/lekrom/criticaldeck/ui/screen/DecksScreen.kt
@@ -4,8 +4,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.lekrom.criticaldeck.model.CriticalDecks
@@ -20,6 +25,15 @@ fun DecksScreen(navController: NavController, decks: CriticalDecks) {
             .fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
+        Text(
+            text = "Critical Decks",
+            style = MaterialTheme.typography.h2,
+            modifier = Modifier
+                .padding(bottom = 48.dp)
+                .align(alignment = Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold
+        )
         decks.decks.forEach {
             Deck(title = it) {
                 navController.navigate(Screen.Cards.withArg(it))
