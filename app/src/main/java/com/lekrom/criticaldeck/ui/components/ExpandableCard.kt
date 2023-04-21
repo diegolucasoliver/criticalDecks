7cdb87d7e1ebd6b1ae015ed9e08dc88f42c7cc28
Diego Oliveira <diegolucasoliver@hotmail.com>
2023-04-21 14:23:15 -0300

    created the expandable card

---
+++ app/src/main/java/com/lekrom/criticaldeck/ui/components/ExpandableCard.kt
@@ -0,0 +1,133 @@
+package com.lekrom.criticaldeck.ui.components
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.lekrom.criticaldeck.ui.theme.CardBackground
+import com.lekrom.criticaldeck.ui.theme.body
+import com.lekrom.criticaldeck.ui.theme.boldBody
+import com.lekrom.criticaldeck.ui.theme.cardTitle
+
+@ExperimentalMaterialApi
+@Composable
+fun ExpandableCard(
+    title: String,
+    content: String? = null,
+    listContent: List<String>? = null
+) {
+    var expandedState by remember { mutableStateOf(false) }
+    val rotationState by animateFloatAsState(
+        targetValue = if (expandedState) 180f else 0f
+    )
+    val shape = RoundedCornerShape(12.dp)
+
+    Card(
+        shape = shape,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 16.dp)
+            .animateContentSize(
+                animationSpec = tween(
+                    durationMillis = 300,
+                    easing = LinearOutSlowInEasing
+                )
+            )
+            .border(
+                width = 1.dp,
+                color = Color.Black,
+                shape = shape
+            ),
+        onClick = {
+            expandedState = !expandedState
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .background(CardBackground)
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = Modifier.weight(6f),
+                    text = title,
+                    style = MaterialTheme.typography.cardTitle
+                )
+                IconButton(
+                    modifier = Modifier
+                        .alpha(ContentAlpha.medium)
+                        .weight(1f)
+                        .rotate(rotationState),
+                    onClick = { expandedState = !expandedState }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = ""
+                    )
+                }
+            }
+
+            if (expandedState) {
+                content?.let {
+                    Text(
+                        text = it,
+                        style = MaterialTheme.typography.body
+                    )
+                }
+                listContent?.let {
+                    it.forEach {
+                        Row {
+                            Text(
+                                text = "\u2022",
+                                style = MaterialTheme.typography.boldBody,
+                                modifier = Modifier.padding(end = 4.dp)
+                            )
+                            Text(
+                                text = it,
+                                style = MaterialTheme.typography.body
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+@Preview
+fun ExpandableCardPreview() {
+    ExpandableCard(title = "Teste")
+}
