3f23c692726ee41d501e485550ddb4c0f517b1f8
Diego Oliveira <diegolucasoliver@hotmail.com>
2023-04-21 14:26:32 -0300

    added the Expandable cards in rules screen

--- app/src/main/java/com/lekrom/criticaldeck/ui/screen/RulesScreen.kt
+++ app/src/main/java/com/lekrom/criticaldeck/ui/screen/RulesScreen.kt
@@ -1,102 +1,54 @@
 package com.lekrom.criticaldeck.ui.screen
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.lekrom.criticaldeck.model.Rules
-import com.lekrom.criticaldeck.ui.theme.body
-import com.lekrom.criticaldeck.ui.theme.boldBody
-import com.lekrom.criticaldeck.ui.theme.cardTitle
+import com.lekrom.criticaldeck.ui.components.ExpandableCard
 
+@ExperimentalMaterialApi
 @Composable
 fun RulesScreen(rules: Rules) {
 
     Column(
         modifier = Modifier
-            .padding(16.dp)
+            .padding(horizontal = 32.dp, vertical = 16.dp)
             .verticalScroll(rememberScrollState())
             .fillMaxSize()
     ) {
 
-        Text(
-            text = "Regras",
-            style = MaterialTheme.typography.cardTitle,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+        ExpandableCard(
+            title = "Gerais",
+            content = rules.description
         )
-        Text(
-            text = rules.description,
-            style = MaterialTheme.typography.body
+
+        ExpandableCard(
+            title = "Especiais",
+            listContent = rules.specials
         )
-        Text(
-            text = "Regras Especiais",
-            style = MaterialTheme.typography.boldBody,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .align(alignment = Alignment.CenterHorizontally)
-                .padding(top = 12.dp)
+
+        ExpandableCard(
+            title = "Variante Mortal",
+            content = rules.mortalVariant
         )
-        rules.specials.forEach {
-            Row {
-                Text(
-                    text = "\u2022",
-                    style = MaterialTheme.typography.boldBody,
-                    modifier = Modifier.padding(end = 4.dp)
-                )
-                Text(
-                    text = it,
-                    style = MaterialTheme.typography.body
-                )
-            }
-        }
-        Text(
-            text = "Variante Mortal",
-            style = MaterialTheme.typography.boldBody,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .align(alignment = Alignment.CenterHorizontally)
-                .padding(top = 12.dp)
-        )
-        Text(
-            text = rules.mortalVariant,
-            style = MaterialTheme.typography.body
-        )
+
         rules.proficiencyVariant?.let {
-            Text(
-                text = "Variante por Proficiência",
-                style = MaterialTheme.typography.boldBody,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .align(alignment = Alignment.CenterHorizontally)
-                    .padding(top = 12.dp)
-            )
-            Text(
-                text = it,
-                style = MaterialTheme.typography.body
+            ExpandableCard(
+                title = "Variante por Proficiência",
+                content = it
             )
         }
+
         rules.criticalSuccessDeckVariant?.let {
-            Text(
-                text = "Variante com Baralho de Acertos Críticos",
-                style = MaterialTheme.typography.boldBody,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .align(alignment = Alignment.CenterHorizontally)
-                    .padding(top = 12.dp)
-            )
-            Text(
-                text = it,
-                style = MaterialTheme.typography.body
+            ExpandableCard(
+                title = "Variante com Baralho de Acertos Críticos",
+                content = it
             )
         }
     }
